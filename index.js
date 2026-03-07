96796932ff79691cddc2dc127f7017b17f220154
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-07 15:54:49 +0100

    Data fetched successfully from the api and the watchlist.html created

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import getApiKey from "./env.js";
 const apiKey = getApiKey();
 
 const baseURL = `http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`;
+
+const dataFromServer = async () => {
+  const response = await fetch(`${baseURL}`);
+  const data = await response.json();
+
+  console.log(data);
+};
+
+// dataFromServer()
