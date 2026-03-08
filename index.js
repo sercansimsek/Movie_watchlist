33187eae636889466ce049536a33b9c0e8dc8edd
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-08 18:20:08 +0100

    fetch cleanup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const searchInput = document.querySelector(".search-input");
 const submitBtn = document.querySelector(".submit-btn");
 const form = document.querySelector(".form");
 const listingContainer = document.querySelector(".listing-container");
+const startExploring = document.querySelector(".start-exploring");
 
 import getApiKey from "./env.js";
 
@@ -10,6 +11,7 @@ const apiKey = getApiKey();
 
 function dataFromServer(event) {
   event.preventDefault();
+  startExploring.style.display = "none";
   let listingHTML = "";
 
   const movieTitle = searchInput.value.trim();
@@ -19,33 +21,42 @@ function dataFromServer(event) {
     return;
   }
 
-  fetch(`https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+  fetch(`https://www.omdbapi.com/?s=${movieTitle}&apikey=${apiKey}`)
+    .then((response) => {
+      response.json();
 
-      listingHTML = `
-        <li class="listing">
+      if (!response.ok) {
+        main.innerHTML =
+          "<p>Unable to find what you are looking for. Please try another search.</p>";
+      }
+    })
+    .then((data) => {
+      data.Search.map((movie) => {
+        fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`)
+          .then((res) => res.json())
+          .then((extraData) => {
+            listingHTML += `
+          <li class="listing">
           <img
-            src="${data.Poster}"
+            src="${movie.Poster}"
             alt="listing banner"
             class="listing-banner"
           />
           <div class="listing-info">
             <div class="listing-header">
-              <p class="listing-title">${data.Title}</p>
+              <p class="listing-title">${movie.Title}</p>
               <div class="listing-rating">
                 <img
                   src="/images/Icon.svg"
                   alt="Filled star represents rating"
                   class="rate-icon"
                 />
-                <p class="listing-rate">${data.Ratings[0].Value}</p>
+                <p class="listing-rate">${extraData.Ratings[0].Value}</p>
               </div>
             </div>
             <div class="listing-extra">
-              <p class="listing-duration">${data.Runtime}</p>
-              <p class="listing-category">${data.Genre}</p>
+              <p class="listing-duration">${extraData.Runtime}</p>
+              <p class="listing-category">${extraData.Genre}</p>
               <div class="listing-watchlist">
                 <img
                   src="/images/dev.svg"
@@ -57,14 +68,19 @@ function dataFromServer(event) {
               </div>
             </div>
             <p class="listing-description">
-              ${data.Plot}
+              ${extraData.Plot}
             </p>
           </div>
         </li>
-        <hr class="divider" 
-      `;
-      listingContainer.innerHTML = listingHTML;
+        <hr class="divider"></hr> 
+        `;
+
+            listingContainer.innerHTML = listingHTML;
+          });
+      });
     });
+
+  searchInput.value = "";
 }
 
 form.addEventListener("submit", dataFromServer);
